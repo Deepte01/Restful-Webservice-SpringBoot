4da7c18c61ef438d62912a5233b6b27eb7317a6d
Deepika Teegapuram <dteegapuram@gmail.com>
2024-08-04 21:04:01 -0600

    added a HTTP Post body to accept XML/JSON

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,23 +1,49 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
+import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
     @GetMapping()
-    public String getUser(@RequestParam(value="page") int page,
-                          @RequestParam(value="limit") int limit){
+    public String getUser(@RequestParam(value="page", defaultValue = "1", required = false) int page,
+                          @RequestParam(value="limit", defaultValue = "50") int limit,
+                          @RequestParam(value = "sort", required = false) String sort){
         return "get user page: " + page + " limit:" + limit;
     }
 
-    @GetMapping(path = "/{userId}")
-    public String getUser(@PathVariable String userId){
-        return "get user was called." + userId;
+    @GetMapping(path = "/{userId}", produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+    })
+    public UserRest getUser(@PathVariable String userId){
+
+        UserRest user = new UserRest();
+        user.setUserId("5");
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setEmail("john@doe.com");
+
+        return user;
     }
-    @PostMapping
-    public String createUser(){
-        return "create user is called.";
+    @PostMapping(consumes = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+    },
+    produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+    })
+    public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel details){
+        UserRest user = new UserRest();
+        user.setFirstName(details.getFirstName());
+        user.setLastName(details.getLastName());
+        user.setEmail(details.getEmail());
+        return  ResponseEntity.ok(user);
     }
     @PutMapping
     public String updateUser()
